d19f4a15c02a02cd740ddeebf23efa95f97e7d5f
GiorgiTurmanidze <fiveanderson44@gmail.com>
2023-05-15 22:15:26 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,90 @@
+// დავალება 2 ===============================================
+
+
+                                                            // TYPE 1
+
+// function f1zz() {
+//     var x = prompt("Enter Number: ")
+//     if(!isNaN(x)) {
+//         if(x % 3 == 0 && x % 5 == 0) {
+//             console.log("fizz buzz")
+//         }
+//         else{
+//             if (x % 3 == 0) {
+//                 console.log("fizz")
+//             }
+//             if (x % 5 == 0) {
+//                 console.log("buzz")
+//             }
+//         }
+        
+//     }
+//     else {
+//         alert("YOU HAVE TO ENTER A NUMBER!")
+//     }
+    
+// }
+// f1zz();
+
+                                                            // TYPE 2
+
+// function f2zz(x) {
+//     if(!isNaN(x)) {
+//         if(x % 3 == 0 && x % 5 == 0) {
+//             console.log("fizz buzz")
+//         }
+//         else{
+//             if (x % 3 == 0) {
+//                 console.log("fizz")
+//             }
+//             if (x % 5 == 0) {
+//                 console.log("buzz")
+//             }
+//         }
+        
+//     }
+//     else {
+//         alert("YOU HAVE TO ENTER A NUMBER!")
+//     }
+    
+// }
+// f2zz(3);
+// ============================================================
+
+// დავალება 3 =================================================
+
+// function question() {
+//     var a = prompt("Enter Number A: ")
+//     var b = prompt("Enter Number B: ")
+//     if(!isNaN(a && b)) {
+//         if (a < b) {
+//             for (let i = a; i <= b; i++) {
+//                 if(i % 2 == 0) {
+//                     a += 1
+                    
+//                 }
+//                 else {
+//                     continue;
+//                 }
+//                 console.log(i)
+//             }
+//         }
+//         if (a > b) {
+//             for (let i = b; i <= a; i++) {
+//                 if(i % 2 == 0) {
+//                     b += 1
+                    
+//                 }
+//                 else {
+//                     continue;
+//                 }
+//                 console.log(i)
+//             }
+//         }
+//     }
+//     else{
+//         alert("YOU HAVE TO ENTER A NUMBER!!!")
+//     }
+    
+// }
+// question();
